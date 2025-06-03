1fc4d340bd22cf315f43740970cd784dee8ce0f6
KHEBACHE Camyl <camyl.khebache2@etu.univ-lorraine.fr>
2025-06-03 14:38:46 +0200

    red

---
+++ minesweeper.test.ts
@@ -0,0 +1,55 @@
+import { assertEquals } from "jsr:@std/assert";
+
+export function minesweeper(minefield: string): string {
+  // if (input === "") return "";
+
+  // const rows = input.split("\n");
+  // const height = rows.length;
+  // const width = rows[0].length;
+
+  // let result: string[] = [];
+
+  // for (let y = 0; y < height; y++) {
+  //   let line = "";
+  //   for (let x = 0; x < width; x++) {
+  //     if (rows[y][x] === "*") {
+  //       line += "*";
+  //     } else {
+  //       let count = 0;
+
+  //       for (let dy = -1; dy <= 1; dy++) {
+  //         for (let dx = -1; dx <= 1; dx++) {
+  //           if (dy === 0 && dx === 0) continue; 
+
+  //           const newY = y + dy;
+  //           const newX = x + dx;
+
+  //           if (
+  //             newY >= 0 &&
+  //             newY < height &&
+  //             newX >= 0 &&
+  //             newX < width &&
+  //             rows[newY][newX] === "*"
+  //           ) {
+  //             count++;
+  //           }
+  //         }
+  //       }
+
+  //       line += count.toString();
+  //     }
+  //   }
+  //   result.push(line);
+  // }
+
+  // return result.join("\n");
+  return "";
+}
+
+
+Deno.test("Case vide", () => {
+  assertEquals(minesweeper("."), "0");
+});
+
+
+
